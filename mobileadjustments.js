38f0563a1d10c779e4ac0835ecfcb548dec67b8d
JinzUtAreZ-Work <83233933+JinzUtAreZ-Work@users.noreply.github.com>
2022-11-18 12:32:18 +0800

    mobile browser adjustments

---
+++ mobileadjustments.js
@@ -0,0 +1,5 @@
+Email in mobile browser
+<li><a style="text-decoration:none" href="mailto:helpline@in-touch.org">helpline@in-touch.org</a></span></li>
+Phone in mobile browser
+<li style="list-style:none"><a style="text-decoration: none;color:inherit;" href="tel:+639178001123">+63 917 800 1123</a></li>
+  
